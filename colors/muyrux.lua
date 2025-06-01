aefca4929626aa511c7249114bb27f18af878bb1
Andrian Hornyak <andrianhornyak@Adrians-MacBook-Pro-14.local>
2025-06-01 13:38:40 +0200

    update nvimtree hi fix

--- colors/muyrux.lua
+++ colors/muyrux.lua
@@ -188,7 +188,7 @@ Group.new("GitSignsDelete", c.red, c.none, s.none)
 Group.new("NvimTreeFolderIcon", c.purple, c.none, s.none)
 Group.new("NvimTreeFolderName", c.Normal, c.none, s.none)
 -- added
-Group.new("NvimTreeGitNewIcon", c.green_deep, c.none, s.none)
+Group.new("NvimTreeGitNew", c.green_deep, c.none, s.none)
 Group.new("NvimTreeGitFileNewHL", c.Normal, c.none, s.none)
 Group.new("NvimTreeGitFolderNewHL", c.Normal, c.none, s.none)
 -- merged
@@ -196,19 +196,19 @@ Group.new("NvimTreeGitMerge", c.purple, c.none, s.none)
 Group.new("NvimTreeGitFileMergeHL", c.Normal, c.none, s.none)
 Group.new("NvimTreeGitFolderMergeHL", c.Normal, c.none, s.none)
 -- dirty
-Group.new("NvimTreeGitDirtyIcon", c.orange, c.none, s.none)
+Group.new("NvimTreeGitDirty", c.orange, c.none, s.none)
 Group.new("NvimTreeGitFileDirtyHL", c.Normal, c.none, s.none)
 Group.new("NvimTreeGitFolderDirtyHL", c.Normal, c.none, s.none)
 -- staged
-Group.new("NvimTreeGitStagedIcon", c.blue, c.none, s.none)
+Group.new("NvimTreeGitStaged", c.blue, c.none, s.none)
 Group.new("NvimTreeGitFileStagedHL", c.Normal, c.none, s.none)
 Group.new("NvimTreeGitFolderStagedHL", c.Normal, c.none, s.none)
 -- renamed
-Group.new("NvimTreeGitRenamedIcon", c.blue_dark, c.none, s.none)
+Group.new("NvimTreeGitRenamed", c.blue_dark, c.none, s.none)
 Group.new("NvimTreeGitFileRenamedHL", c.Normal, c.none, s.none)
 Group.new("NvimTreeGitFolderRenamedHL", c.Normal, c.none, s.none)
 -- deleted
-Group.new("NvimTreeGitDeletedIcon", c.red, c.none, s.none)
+Group.new("NvimTreeGitDeleted", c.red, c.none, s.none)
 Group.new("NvimTreeGitFileDeletedHL", c.Normal, c.none, s.none)
 Group.new("NvimTreeGitFolderDeletedHL", c.Normal, c.none, s.none)
 
