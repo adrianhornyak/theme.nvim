679c9816e2ee88eec73d336f404446f5bc1c55f4
Andrian Hornyak <andrianhornyak@Adrians-MacBook-Pro-14.local>
2025-09-06 18:26:35 +0200

    hi visual

--- colors/muyrux.lua
+++ colors/muyrux.lua
@@ -1,1 +1,18 @@
-require("muyrux").load(vim.o.background)
+local M = {}
+
+M.config = function()
+  local muyrux = require("plugins.muyrux.colors")
+  -- Автозагрузка темы при старте Neovim
+  muyrux.load(vim.o.background, false)
+
+  -- Команды для toggle
+  vim.api.nvim_create_user_command("MuyruxToggleTheme", function()
+    muyrux.toggle()
+  end, {})
+
+  vim.api.nvim_create_user_command("MuyruxToggleTransparent", function()
+    muyrux.toggle_transparent()
+  end, {})
+end
+
+return Mbackground)
